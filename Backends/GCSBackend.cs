44e47ed4785d0c1b15b4e3f4677eb010c9088189
Wojciech Maryszczak <wojciech.maryszczak@anixe.pl>
2018-07-26 16:57:16 +0200

    Add google cloud storage and mongodb deps

--- Backends/GCSBackend.cs
+++ Backends/GCSBackend.cs
@@ -1,14 +1,25 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
+using Google.Cloud.Storage.V1;
 
 namespace log_forwarder.Backends
 {
   public class GCSBackend : IBackend
   {
+    private readonly StorageClient client;
+
+    public GCSBackend()
+    {
+      this.client = this.client = StorageClient.Create();
+    }
+
     public Task SendAsync(Stream stream, Dictionary<string, string> options)
     {
-      // using()
+      var bucketName = options["bucket"];
+      var fileName = options["filename"];
+      var contentType = options["content_type"];
+      return client.UploadObjectAsync(bucketName, fileName, contentType, stream);
     }
   }
 }
